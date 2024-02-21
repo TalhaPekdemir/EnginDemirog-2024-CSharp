19c1d0c47355876eca405cde5ac0afbb3c2c7542
TalhaPekdemir <75481622+TalhaPekdemir@users.noreply.github.com>
2024-02-21 14:38:49 +0300

    Add project files.

---
+++ Intro/Business/CourseManager.cs
@@ -0,0 +1,35 @@
+﻿using Intro.Entities;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses;
+    public CourseManager()
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C# Zero-to-Hero";
+        course1.Description = ".NET 8";
+        course1.Price = 19.99;
+
+        Course course2 = new Course();
+        course2.Id = 2;
+        course2.Name = "Data Science with Python";
+        course2.Description = "TensorFlow 2.15";
+        course2.Price = 99.99;
+
+        Course course3 = new Course();
+        course3.Id = 3;
+        course3.Name = "Java is not legacy code!!";
+        course3.Description = "Java 17";
+        course3.Price = 29.99;
+
+        courses = [course1, course2, course3];
+    }
+
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,73 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+namespace Intro;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // variables -> camelCase
+        string message = "Krediler";
+        int term = 12;
+        double amount = 100000;
+        bool isAuthenticated = false;
+
+        Console.WriteLine(message);
+
+        // conditions
+        if(isAuthenticated ) 
+        {
+            Console.WriteLine("Buton->Hoşgeldin Talha");
+        }
+        else
+        {
+            Console.WriteLine("Buton->Sisteme Giris Yap");
+        }
+
+        // arrays
+        string[] loans = { "Kredi1", "Kredi2", "Kredi3", "Kredi4", "Kredi5" };
+        //string[] loans = new string[5];
+        //loans[0] = "Kredi1";
+
+        // for loop
+        for(int i = 0; i < loans.Length; i++)
+        {
+            Console.WriteLine(loans[i]);
+        }
+
+        // foreach
+        foreach(string loan in loans)
+        {
+            Console.WriteLine(loan);
+        }
+
+        // while
+        int iterator = 0;
+        while(iterator < loans.Length)
+        {
+            Console.WriteLine(loans[iterator]);
+            iterator++;
+        }
+
+        iterator = 0;
+        // do-while
+        do
+        {
+            Console.WriteLine(loans[iterator]);
+            iterator++;
+        }
+        while(iterator < loans.Length);
+
+        // OOP
+        CourseManager courseManager = new CourseManager();
+        Course[] courses = courseManager.GetAll();
+
+        for(int i = 0; i < courses.Length; i++)
+        {
+            Console.WriteLine($"Kurs adı: {courses[i].Name} Kurs Fiyatı: {courses[i].Price}");
+        }
+
+        Console.WriteLine("Kod Bitti");
+    }
+}
